61df1b745981597999923ce60c26443d3b7a6042
Matthew Rueben <mrueben@nmsu.edu>
2026-01-09 22:48:55 -0800

    Add code from student-grouper repo.

---
+++ src/GroupingSearch.java
@@ -0,0 +1,8 @@
+import java.util.List;
+
+public interface GroupingSearch
+{
+    public void submitValidGrouping(List<List<Integer>> grouping);
+    public boolean backtrackingIsOn();
+    public boolean shouldBacktrack(List<Integer> groupSoFar);
+}
